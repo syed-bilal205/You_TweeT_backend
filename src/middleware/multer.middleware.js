340df29fa8b68d52b5487d31eb091bd1db8f6536
Syed Bilal <syedb4115@gmail.com>
2024-07-17 17:50:57 +0500

    try to resolve the error from multer

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,8 +1,13 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 // Resolve the temporary directory path
 const tempDir = path.resolve("./public/temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
