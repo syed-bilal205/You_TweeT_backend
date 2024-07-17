bb8072ed07e22113f1f108a5581c44a28b7ed281
Syed Bilal <syedb4115@gmail.com>
2024-07-17 15:50:41 +0500

    after deployment some changes

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,8 +1,16 @@
+import fs from "fs";
 import multer from "multer";
+import path from "path";
+
+const tempDir = path.join(__dirname, "public/temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
