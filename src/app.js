deace85bd9b301b409fd50fa7ba9116453a799d6
Syed Bilal <syedb4115@gmail.com>
2024-07-17 19:05:55 +0500

    commit

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      console.log(`Origin: ${origin}`);
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
